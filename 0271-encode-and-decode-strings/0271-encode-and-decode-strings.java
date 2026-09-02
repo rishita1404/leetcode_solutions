41ddfd33448d5c67536bdc4243810f9b8a8273b6
rishita1404 <rishitag1404@gmail.com>
2026-09-03 01:10:46 +0530

    Time: 4 ms (85.24%), Space: 44.2 MB (78.36%) - LeetHub

---
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
@@ -0,0 +1,37 @@
+﻿class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder result=new StringBuilder();
+        for(String s:strs){
+            int n=s.length();
+            result.append(n);
+            result.append(',');
+        }
+        result.append('#');
+        for(String s:strs){
+            result.append(s);
+        }
+        return result.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> res=new ArrayList<>();
+        ArrayList<Integer> lens=new ArrayList<>();
+        int i=0;
+        while(str.charAt(i)!='#'){
+            StringBuilder num=new StringBuilder();
+            while(str.charAt(i)!=','){
+                num.append(str.charAt(i));
+                i++;
+            }
+            lens.add(Integer.parseInt(num.toString()));
+            i++;
+        }
+        i++;
+        for(int len:lens){
+            res.add(str.substring(i, i+len));
+            i+=len;
+        }
+        return res;
+    }
+}
