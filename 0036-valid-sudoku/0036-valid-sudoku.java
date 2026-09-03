48d9b21ec2c7594afbc92f866e4ff4ee866c42d9
rishita1404 <rishitag1404@gmail.com>
2026-09-03 14:32:57 +0530

    Time: 2 ms (62.76%), Space: 46.3 MB (69.07%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for(int i=0; i<9; i++){
+            HashSet<Character> set=new HashSet<>();
+            for(int j=0; j<9; j++){
+                if(board[i][j]!='.'){
+                    if(!set.add(board[i][j])){
+                        return false;
+                    }
+                }
+            }
+        }
+        for(int i=0; i<9; i++){
+            HashSet<Character> set=new HashSet<>();
+            for(int j=0; j<9; j++){
+                if(board[j][i]!='.'){
+                    if(!set.add(board[j][i])){
+                        return false;
+                    }
+                }
+            }
+        }
+        for(int row=0; row<9; row+=3){
+            for(int col=0; col<9; col+=3){
+                HashSet<Character> set=new HashSet<>();
+                for(int i=row; i<row+3; i++){
+                    for(int j=col; j<col+3; j++){
+                        if(board[i][j]!='.'){
+                            if(!set.add(board[i][j])){
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
